f531015fe4fd5f2f2cf7e0e2d8e7e2a903de522d
Tobechukwu <tobechipaschal@gmail.com>
2022-06-01 06:54:18 +0100

    update

--- server/client/product_page.js
+++ server/client/product_page.js
@@ -30,10 +30,106 @@ function getAllMeals () {
     $.ajax({
         url: "server/controller/product.php",
         method: "POST",
-        data: { action: "getMenu", category: "Breakfast" },
+        data: { action: "getMenu", category: "breakfast" },
         success: function (data) {
             // console.log(data)
             document.getElementById("breakfastMenu").innerHTML += data;
+        },
+
+        error: function (e) {
+            $("#output").text(e.responseText);
+            console.log("ERROR : ", e);
+            $("#place-order").prop("disabled", false);
+        }
+    });
+
+    $.ajax({
+        url: "server/controller/product.php",
+        method: "POST",
+        data: { action: "getMenu", category: "lunch" },
+        success: function (data) {
+            // console.log(data)
+            document.getElementById("lunchMenu").innerHTML += data;
+        },
+
+        error: function (e) {
+            $("#output").text(e.responseText);
+            console.log("ERROR : ", e);
+            $("#place-order").prop("disabled", false);
+        }
+    });
+
+    $.ajax({
+        url: "server/controller/product.php",
+        method: "POST",
+        data: { action: "getMenu", category: "dinner" },
+        success: function (data) {
+            // console.log(data)
+            document.getElementById("dinnerMenu").innerHTML += data;
+        },
+
+        error: function (e) {
+            $("#output").text(e.responseText);
+            console.log("ERROR : ", e);
+            $("#place-order").prop("disabled", false);
+        }
+    });
+
+    $.ajax({
+        url: "server/controller/product.php",
+        method: "POST",
+        data: { action: "getMenu", category: "keto" },
+        success: function (data) {
+            // console.log(data)
+            document.getElementById("ketoMenu").innerHTML += data;
+        },
+
+        error: function (e) {
+            $("#output").text(e.responseText);
+            console.log("ERROR : ", e);
+            $("#place-order").prop("disabled", false);
+        }
+    });
+
+    $.ajax({
+        url: "server/controller/product.php",
+        method: "POST",
+        data: { action: "getMenu", category: "plant" },
+        success: function (data) {
+            // console.log(data)
+            document.getElementById("plantMenu").innerHTML += data;
+        },
+
+        error: function (e) {
+            $("#output").text(e.responseText);
+            console.log("ERROR : ", e);
+            $("#place-order").prop("disabled", false);
+        }
+    });
+
+    $.ajax({
+        url: "server/controller/product.php",
+        method: "POST",
+        data: { action: "getMenu", category: "drinks" },
+        success: function (data) {
+            // console.log(data)
+            document.getElementById("drinksMenu").innerHTML += data;
+        },
+
+        error: function (e) {
+            $("#output").text(e.responseText);
+            console.log("ERROR : ", e);
+            $("#place-order").prop("disabled", false);
+        }
+    });
+
+    $.ajax({
+        url: "server/controller/product.php",
+        method: "POST",
+        data: { action: "getMenu", category: "meal_deals" },
+        success: function (data) {
+            // console.log(data)
+            document.getElementById("meals_dealsMenu").innerHTML += data;
         },
 
         error: function (e) {
